fabd18526ce209c54b4a243c74533e0576d11054
Helen <helenmayanna@gmail.com>
2017-03-05 17:18:32 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,46 +1,96 @@
 class GroupsController < ApplicationController
-  def index
-    @groups = Group.all
-  end
+    def index
+        @groups = Group.all
+    end
 
-  def show
-    @group = Group.find(params[:id])
-  end
+    def show
+        @group = Group.find(params[:id])
+    end
 
-  def new
-    @group = Group.new
-  end
+    def new
+        @group = Group.new
+    end
 
-  def edit
-    @group = Group.find(params[:id])
-  end
+    def edit
+        @group = Group.find(params[:id])
+    end
 
-  def create
-    @group = Group.new(group_params)
-    @group.save
+    def create
+        @group = Group.new(group_params)
 
-    redirect_to groups_path
-  end
+        if @group.save
+            redirect_to groups_path
+        else
+            render :new
+        end
+      end
 
-  def update
-    @group = Group.find(params[:id])
+    def update
+        @group = Group.find(params[:id])
 
-    @group.update(group_params)
+        @group.update(group_params)
 
-    redirect_to groups_path, notice: "Update Success"
-  end
+        redirect_to groups_path, notice: 'Update Success'
+    end
 
-  def destroy
-    @group = Group.find(params[:id])
-    @group.destroy
-    flash[:alert] = "Group deleted"
-    redirect_to groups_path
-  end
+    def destroy
+        @group = Group.find(params[:id])
+        @group.destroy
+        flash[:alert] = 'Group deleted'
+        redirect_to groups_path
+    end
 
-  private
+    private
 
-  def group_params
-    params.require(:group).permit(:title, :description)
-  end
+    def group_params
+        params.require(:group).permit(:title, :description)
+    end
+end
+class GroupsController < ApplicationController
+    def index
+        @groups = Group.all
+    end
 
+    def show
+        @group = Group.find(params[:id])
+    end
+
+    def new
+        @group = Group.new
+    end
+
+    def edit
+        @group = Group.find(params[:id])
+    end
+
+    def create
+        @group = Group.new(group_params)
+
+        if @group.save
+            redirect_to groups_path
+        else
+            render :new
+        end
+      end
+
+    def update
+        @group = Group.find(params[:id])
+
+        @group.update(group_params)
+
+        redirect_to groups_path, notice: 'Update Success'
+    end
+
+    def destroy
+        @group = Group.find(params[:id])
+        @group.destroy
+        flash[:alert] = 'Group deleted'
+        redirect_to groups_path
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:title, :description)
+    end
 end
